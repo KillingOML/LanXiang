f9ced03009abfe8f01fd61de3a7185e0570a69e8
Susong <yixin@11111111.local>
2014-10-08 15:50:47 +0800

    from lanxiang
    my name is xiaotao

---
+++ Ogle/Ogle/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Ogle
+//
+//  Created by 宜信 on 13-11-6.
+//  Copyright creditease 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Ogle/Ogle/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ogle
+//
+//  Created by 宜信 on 13-11-6.
+//  Copyright creditease 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
